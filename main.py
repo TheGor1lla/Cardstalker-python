60758e8f441e2ae93a1f901a844106ddc24ced8d
Lord Helm <github@gor1lla.de>
2020-11-19 23:03:11 +0100

    WIP: replace csv with TinyDB

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import csv
-import os
 import uuid
 
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+db = TinyDB('./db.json')
 
 
 @app.route('/check_card')
 def check_card():
     # nimmt die erste karte
-    #holt sich min preis
-    #erhöht min preis um X
-    #checkt ob aktueller preis < min preis
+    # holt sich min preis
+    # erhöht min preis um X
+    # checkt ob aktueller preis < min preis
     # wenn:
     # print()
     with open('cardlist.csv', 'rt') as inp:
@@ -27,24 +28,18 @@ def check_card():
         lowest_price = html.find("dt", text=["From", "ab"]).findNext("dd").string
         lowest_price = convert_to_float(lowest_price)
         if lowest_price < 300:
-            print("Neuer Tiefstpreis: "+ str(lowest_price)+"€")
-
+            print("Neuer Tiefstpreis: " + str(lowest_price) + "€")
 
     return "nice"
 
 
 @app.route('/delete/<card_id>')
 def delete_card(card_id):
-    with open('cardlist.csv', 'rt') as inp, open('first_edit.csv', 'wt', newline='') as out:
-        writer = csv.writer(out)
-        for row in csv.reader(inp):
-            if row[6] != card_id:
-                writer.writerow(row)
-            else:
-                cardname = row[0]
-    os.remove('cardlist.csv')
-    os.rename('first_edit.csv', 'cardlist.csv')
-    return "{} deleted".format(cardname)
+    query = Query()
+
+    card_to_delete = db.get(query.uuid == card_id)
+    db.remove(query.uuid == card_id)
+    return "{} deleted".format(card_to_delete.get('cardLink'))
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -57,39 +52,37 @@ def card_stalker():
 
 
 def save_details():
-    cardlink = get_base_url(request.form.get("cardlink"))
     locale = request.form.get("locale")
     foil = request.form.get("foil")
     mail = request.form.get("mail")
-    price = request.form.get("price")
+    notification_price = request.form.get("price")
     unique_id = str(uuid.uuid4())
+    card_link = get_base_url(request.form.get("cardlink")) + '?language={}&isFoil={}'.format(locale, foil)
 
-    with open('cardlist.csv', mode='a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    # grab current price
+    response = requests.get(card_link)
+    html = BeautifulSoup(response.text, 'html.parser')
+    lowest_price = html.find("dt", text=["From", "ab"]).findNext("dd").string
+    lowest_price = convert_to_float(lowest_price)
 
-        if os.stat("cardlist.csv").st_size == 0:
-            fields = ['cardlink', 'locale', 'foil', 'mail', 'lowestRecordedPrice', 'maxReactionPrice', 'uuid']
-            csv_writer.writerow(fields)
-
-        #grab current price
-        response = requests.get(get_filtered_card(cardlink, locale, foil))
-        html = BeautifulSoup(response.text, 'html.parser')
-        lowest_price = html.find("dt", text=["From", "ab"]).findNext("dd").string
-        lowest_price = convert_to_float(lowest_price)
-
-        csv_writer.writerow([cardlink, locale, foil, mail, lowest_price, price, unique_id])
+    db.insert({'cardLink': card_link,
+               'locale': locale,
+               'foil': foil,
+               'mail': mail,
+               'notificationPrice': notification_price,
+               'uuid': unique_id,
+               'lowestPrice': lowest_price})
 
 
 def get_base_url(url):
     return url.split('?')[0]
 
-def get_filtered_card(cardlink, locale, foil):
-    return (cardlink + '?language={}&isFoil={}'.format(locale, foil))
 
 def convert_to_float(string):
     string = string[:-2]
     price = float(string.replace(',', '.'))
     return price
+
 
 if __name__ == '__main__':
     app.run()
